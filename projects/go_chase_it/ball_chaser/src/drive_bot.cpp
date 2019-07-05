96de1e9c8d49e2cc63650aa9a0b8ac50ae8ab211
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-07-05 11:22:43 -0700

    Implemented and tested drive bot node.

---
+++ projects/go_chase_it/ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,36 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "ball_chaser/DriveToTarget.h"
+#include <string>
+
+ros::Publisher motor_command_publisher;
+
+bool handle_drive_request(ball_chaser::DriveToTarget::Request &request,
+    ball_chaser::DriveToTarget::Response &response)
+{
+  geometry_msgs::Twist motor_command;
+  motor_command.linear.x = request.linear_x;
+  motor_command.angular.z = request.angular_z;
+  motor_command_publisher.publish(motor_command);
+
+  response.msg_feedback = "Commanded robot: linear x = " + std::to_string(request.linear_x)
+    + " , angular z = " + std::to_string(request.angular_z);
+  ROS_INFO_STREAM(response.msg_feedback);
+
+  return true;
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "drive_bot");
+  ros::NodeHandle n;
+
+  motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+  
+  ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot",
+      handle_drive_request);
+
+  ros::spin();
+
+  return 0;
+}
