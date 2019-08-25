8a24e88238d49f7a4b9c8a2682455798a322c6ca
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-08-25 13:14:09 -0700

    Added SLAM-generated maps.

--- projects/home_service_robot/scripts/test_navigation.sh
+++ projects/home_service_robot/scripts/test_navigation.sh
@@ -5,12 +5,12 @@ SOURCE_CATKINWS="source ~/catkin_ws/devel/setup.bash"
 
 # Deploy turtlebot in Gazebo
 xterm -e "$SOURCE_CATKINWS && roslaunch turtlebot_gazebo turtlebot_world.launch \
-  world_file:=$(dirname $(pwd))/worlds/my_world_simple.world" &
+  world_file:=$(dirname $(pwd))/worlds/earthquake.world" &
 sleep 20
 
 # AMCL
 xterm -e "$SOURCE_CATKINWS && roslaunch turtlebot_gazebo amcl_demo.launch \
-  map_file:=$(dirname $(pwd))/maps/my_world_simple_pgmcreator.yaml" &
+  map_file:=$(dirname $(pwd))/maps/earthquake_slam.yaml" &
 sleep 5
 
 # RViz
