a2199f203f312f20faf3e3dde02e9b9241df5649
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-08-24 18:57:08 -0700

    Added shell scripts for testing.

---
+++ projects/home_service_robot/scripts/test_slam.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Variable for sourcing catkin workspace setup file
+SOURCE_CATKINWS="source ~/catkin_ws/devel/setup.bash"
+
+# Deploy turtlebot in Gazebo
+xterm -e "$SOURCE_CATKINWS && roslaunch turtlebot_gazebo turtlebot_world.launch \
+  world_file:=$(rospack find my_robot)/worlds/my_world_simple.world" &
+sleep 15
+
+# SLAM
+xterm -e "$SOURCE_CATKINWS && roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+
+# RViz
+xterm -e "$SOURCE_CATKINWS && roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+# Teleop
+xterm -e "$SOURCE_CATKINWS && roslaunch turtlebot_teleop keyboard_teleop.launch" &
