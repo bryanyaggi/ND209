49b04fd3ce6ecfc4205be8e90bd57c834b0a0b80
Bryan Yaggi <bryan.yaggi@gmail.com>
2019-08-27 00:24:46 -0700

    Navigating to pick-up and drop-off location goals. Need to adjust orientation.

--- projects/home_service_robot/pick_objects/src/pick_objects.cpp
+++ projects/home_service_robot/pick_objects/src/pick_objects.cpp
@@ -1,6 +1,25 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <string>
+#include <vector>
+
+class Goal
+{
+  public:
+    std::string name;
+    double x;
+    double y;
+    double w;
+
+    Goal(std::string nameVal, double xVal, double yVal, double wVal)
+    {
+      name = nameVal;
+      x = xVal;
+      y = yVal;
+      w = wVal;
+    }
+};
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -18,22 +37,34 @@ int main(int argc, char** argv)
   move_base_msgs::MoveBaseGoal goal;
 
   goal.target_pose.header.frame_id = "map";
-  goal.target_pose.header.stamp = ros::Time::now();
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
+  std::vector<Goal> goals;
+  goals.push_back(Goal("pick-up", 2.0, 2.0, 1.0));
+  goals.push_back(Goal("drop-off", -2.0, -2.0, 1.0));
 
-  ac.waitForResult();
-
-  if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+  for (auto& element : goals)
   {
-    ROS_INFO("Hooray, the base moved 1 meter forward");
-  }
-  else
-  {
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = element.x;
+    goal.target_pose.pose.position.y = element.y;
+    goal.target_pose.pose.orientation.w = element.w;
+    
+    ROS_INFO("Sending %s location goal.", element.name.c_str());
+    ac.sendGoal(goal);
+
+    ac.waitForResult();
+
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      ROS_INFO("Reached %s location.", element.name.c_str());
+    }
+    else
+    {
+      ROS_INFO("Failed to reach %s location.", element.name.c_str());
+      return 0;
+    }
+
+    ros::Duration(5.0).sleep();
   }
 
   return 0;
